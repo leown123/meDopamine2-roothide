a13ed8ea621840c0e68fdf3438c6d3243d3dc6c9
leown123 <136781210+leown123@users.noreply.github.com>
2025-05-14 23:46:29 +0800

    Update jbdomain_systemwide.m

--- BaseBin/launchdhook/src/jbserver/jbdomain_systemwide.m
+++ BaseBin/launchdhook/src/jbserver/jbdomain_systemwide.m
@@ -498,6 +498,8 @@ static int systemwide_process_hacktask(audit_token_t *processToken, char **rootP
  	JBLogDebugnew3("本地add： task_for_pid_count ：%ld , task_for_pid_caller_count ：%ld",task_for_pid_count,task_for_pid_caller_count);
 
  	if (proc)  proc_rele(proc);
+
+  	return 0;
 }
 
 static int systemwide_process_checkinnew(audit_token_t *processToken, char **rootPathOut, char **bootUUIDOut, char **sandboxExtensionsOut, bool *fullyDebuggedOut)
